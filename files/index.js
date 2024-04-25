e378cf025e659e6135ba421b94f41709a16e540b
Dan Retegan <danretegan@yahoo.com>
2024-04-25 11:21:09 +0300

    files/index.js update

--- files/index.js
+++ files/index.js
@@ -1,27 +1,41 @@
-//TODO Citeste lista fisierelor din director si creeaza un fisier nou cu lista lor in format CSV
+// TODO: Citeste lista fisierelor dintr-un director si creeaza un fisier nou, in format CSV, cu lista lor:
 
 import { promises as fs } from "fs";
 
-const folderName = "./";
+const folderName = "./test";
 const filename = "log.csv";
 
-await logFilesInDirectory(folderName);
-
-async function logFilesInDirectory() {
+async function main() {
   try {
+    // Apelarea funcției logFilesInDirectory pentru a scrie lista de fișiere în fișierul CSV
+    await logFilesInDirectory(folderName);
+    console.log("Fisierul a fost scris cu succes.");
+
+    // Citirea conținutului fișierului log.csv
+    const content = await fs.readFile(filename, "utf-8");
+    console.log("Conținutul fișierului log.csv:");
+    console.log(content);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Definirea funcției pentru a scrie lista de fișiere în fișierul CSV
+async function logFilesInDirectory(folderName) {
+  try {
+    // Citirea listei de fișiere din directorul specificat
     const files = await fs.readdir(folderName);
+    // Construirea textului pentru fișierul CSV, excluzând fișierele care nu sunt de tipul .txt
     const fileText = files
       .filter((file) => file.includes(".txt"))
       .map((file) => file.replace(".txt", ""))
       .join(",");
 
-    try {
-      fs.writeFile(filename, fileText);
-      console.log("Fisierul a fost scris cu succes.");
-    } catch (error) {
-      console.error(error);
-    }
+    // Scrierea textului în fișierul CSV
+    await fs.writeFile(filename, fileText);
   } catch (error) {
-    console.error(error);
+    throw new Error(`Error while logging files: ${error.message}`);
   }
 }
+
+main();
