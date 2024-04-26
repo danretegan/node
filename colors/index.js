f2215b6fde582beb505163112d98bd57eb6cb7fc
Dan Retegan <danretegan@yahoo.com>
2024-04-26 16:00:20 +0300

    colors

---
+++ colors/index.js
@@ -0,0 +1,60 @@
+import colors from "colors";
+
+console.log("\ntext colors:".underline.bold.italic);
+console.log("black".black);
+console.log("red".red);
+console.log("green".green);
+console.log("yellow".yellow);
+console.log("blue".blue);
+console.log("magenta".magenta);
+console.log("cyan".cyan);
+console.log("white".white);
+console.log("gray".gray);
+console.log("grey".grey);
+
+console.log("\nbright text colors:".underline.bold.italic);
+console.log("brightRed".brightRed);
+console.log("brightGreen".brightGreen);
+console.log("brightYellow".brightYellow);
+console.log("brightBlue".brightBlue);
+console.log("brightMagenta".brightMagenta);
+console.log("brightCyan".brightCyan);
+console.log("brightWhite".brightWhite);
+
+console.log("\nbackground colors:".underline.bold.italic);
+console.log("bgBlack".bgBlack);
+console.log("bgRed".bgRed);
+console.log("bgGreen".bgGreen);
+console.log("bgYellow".bgYellow);
+console.log("bgBlue".bgBlue);
+console.log("bgMagenta".bgMagenta);
+console.log("bgCyan".bgCyan);
+console.log("bgWhite".bgWhite);
+console.log("bgGray".bgGray);
+console.log("bgGrey".bgGrey);
+
+console.log("\nbright background colors:".underline.bold.italic);
+console.log("bgBrightRed".bgBrightRed);
+console.log("bgBrightGreen".bgBrightGreen);
+console.log("bgBrightYellow".bgBrightYellow);
+console.log("bgBrightBlue".bgBrightBlue);
+console.log("bgBrightMagenta".bgBrightMagenta);
+console.log("bgBrightCyan".bgBrightCyan);
+console.log("bgBrightWhite".bgBrightWhite);
+
+console.log("\nstyles:".underline.bold.italic);
+console.log("reset".reset);
+console.log("bold".bold);
+console.log("dim".dim);
+console.log("italic".italic);
+console.log("underline".underline);
+console.log("inverse".inverse);
+console.log("hidden (textul care urmeaza este ascuns):", "HIDDEN".hidden);
+console.log("strikethrough".strikethrough);
+
+console.log("\nextras:".underline.bold.italic);
+console.log("Rainbow".rainbow);
+console.log("Zebra".zebra);
+console.log("America".america);
+console.log("Trap".trap);
+console.log("Random".random);
