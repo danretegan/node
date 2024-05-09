e9e28dfc95d44bb824355deeaf4e0cf2f793632d
Dan Retegan <danretegan@yahoo.com>
2024-05-09 11:40:11 +0300

    update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -5,15 +5,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact page</h1>");
+app.get("/contact/:id", (req, res) => {
+  res.send(`<h1>Contact page</h1> Am introdus parametrul: ${req.params.id}`);
 });
 
+//TODO Middleware:
 app.use((req, res, next) => {
   console.log("\x1b[34mMiddleware-ul nostru\x1b[0m"); //text cu albastru
   next();
 });
 
+//TODO Pentru a porni serverul se apelează metoda app.listen() căruia i se transmite numărul portului:
 app.listen(3000, () => {
   console.log("\x1b[31mExample app listening on port 3000!\x1b[0m"); //text cu rosu
+});
+
+app.patch("/user/:userid", (req, res) => {
+  const id = req.params.userid;
+  // efectuează acțiunile necesare
 });
