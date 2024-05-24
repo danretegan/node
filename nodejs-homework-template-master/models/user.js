3753cb4937152f1b36c1284597b7ee800105dc81
Dan Retegan <danretegan@yahoo.com>
2024-05-24 18:22:34 +0300

    Create user.js

---
+++ nodejs-homework-template-master/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const schema = new Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ["buyer", "seller"],
+    default: "buyer",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+const User = model("User", schema);
+
+export default User;
