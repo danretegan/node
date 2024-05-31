f1f43177f8e333d4d5cde1f98ff940a66e26e7f3
Dan Retegan <danretegan@yahoo.com>
2024-05-31 09:12:43 +0300

    Update pow.test.js

--- conspect/test-jest/pow.test.js
+++ conspect/test-jest/pow.test.js
@@ -36,11 +36,13 @@ describe("hooks", function () {
 
   test("1 to power 2 to equal 1", () => {
     console.log(colors.bgCyan.bold.black("Test 1: 1 to power 2 equal 1"));
+
     expect(pow(1, 2)).toBe(1);
   });
 
   test("3 to power 2 to equal 9", () => {
     console.log(colors.bgRed.bold.white("Test 2: 3 to power 2 equal 9"));
+
     expect(pow(3, 2)).toBe(9);
   });
 });
