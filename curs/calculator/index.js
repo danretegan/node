cdbbd39319ecc82ea733799e9cc7839546ba5cfc
Dan Retegan <danretegan@yahoo.com>
2024-05-31 15:58:17 +0300

    add divide

--- curs/calculator/index.js
+++ curs/calculator/index.js
@@ -1,7 +1,9 @@
 const add = (a, b) => a + b;
+const divide = (a, b) => a / b;
 
 const CalculatorService = {
   add: add,
+  divide: divide,
 };
 
 export default CalculatorService;
--- curs/calculator/index.test.js
+++ curs/calculator/index.test.js
@@ -4,4 +4,8 @@ describe("Calculator", () => {
   it("adds up", () => {
     expect(CalculatorService.add(1, 2)).toBe(3);
   });
+
+  it("divide two numbers", () => {
+    expect(CalculatorService.divide(4, 2)).toBe(2);
+  });
 });
