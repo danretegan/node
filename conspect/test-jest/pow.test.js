5e0a29b06fe77ea076ca8e69bc149662318b3780
Dan Retegan <danretegan@yahoo.com>
2024-05-31 09:11:27 +0300

    add colors

--- conspect/test-jest/pow.test.js
+++ conspect/test-jest/pow.test.js
@@ -1,31 +1,46 @@
+const colors = require("colors");
 const pow = require("./pow");
 
 describe("hooks", function () {
   beforeAll(() => {
-    console.log("1.Rularea codului necesar INAINTEA TUTUROR testelor.");
+    console.log(
+      colors.bgBlue.bold.white(
+        "1. Rularea codului necesar INAINTEA TUTUROR testelor."
+      )
+    );
   });
 
   afterAll(() => {
-    console.log("2.Rularea codului necesar DUPA finalizarea TUTUROR testelor.");
+    console.log(
+      colors.bgMagenta.bold.white(
+        "2. Rularea codului necesar DUPA finalizarea TUTUROR testelor."
+      )
+    );
   });
 
   beforeEach(() => {
-    console.log("3.Rularea codului necesar INAINTEA FIECARUI test.");
+    console.log(
+      colors.bgGreen.bold.white(
+        "3. Rularea codului necesar INAINTEA FIECARUI test."
+      )
+    );
   });
 
   afterEach(() => {
-    console.log("4.Rularea codului necesar DUPA finalizarea FIECARUI test.");
+    console.log(
+      colors.bgYellow.bold.black(
+        "4. Rularea codului necesar DUPA finalizarea FIECARUI test."
+      )
+    );
   });
 
   test("1 to power 2 to equal 1", () => {
-    console.log("1 to power 2 to equal 1");
-
+    console.log(colors.bgCyan.bold.black("Test 1: 1 to power 2 equal 1"));
     expect(pow(1, 2)).toBe(1);
   });
 
   test("3 to power 2 to equal 9", () => {
-    console.log("3 to power 2 to equal 9");
-
+    console.log(colors.bgRed.bold.white("Test 2: 3 to power 2 equal 9"));
     expect(pow(3, 2)).toBe(9);
   });
 });
