8863c33c7866246622ed8ef91da6559999e4e6a3
Dan Retegan <danretegan@yahoo.com>
2024-06-06 11:55:55 +0300

    findOne user with verified email
    Tinem cont de faptul că utilizatorul nu va putea să se autentifice până când adresa lui de e-mail nu a fost verificată. Ne folosim de 'verify: true':

--- nodejs-homework-template-master/controller/authController.js
+++ nodejs-homework-template-master/controller/authController.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import User from "../models/user.js";
 import passport from "passport";
 import gravatar from "gravatar";
+import { v4 as uuidv4 } from "uuid";
 
 const secretForToken = process.env.TOKEN_SECRET;
 
@@ -13,7 +14,14 @@ async function login(data) {
 
   const { email, password } = data;
 
-  const user = await User.findOne({ email });
+  //* Trebuie să se țină cont de faptul că utilizatorul nu va putea să se autentifice până când adresa lui de e-mail nu a fost verificată. Deci adaugam si 'verify: true':
+  const user = await User.findOne({ email: email, verify: true });
+
+  if (!user) {
+    throw new Error(
+      "The username does not exist or the email was not yet validated!"
+    );
+  }
 
   const isMatching = await bcrypt.compare(password, user.password);
 
@@ -43,18 +51,20 @@ async function signup(data) {
 
   const saltRounds = 10;
   const encryptedPassword = await bcrypt.hash(data.password, saltRounds);
-
-  //! Setez noua proprietate AVATAR:
   const userAvatar = gravatar.url(data.email);
+
+  //! implementez o functie care sa genereze un token:
+  const token = uuidv4();
 
   const newUser = new User({
     email: data.email,
     password: encryptedPassword,
     role: "buyer",
     token: null,
-
-    //! Adaug si setez URL-ul avatarului la valoarea generată:
     avatarURL: userAvatar,
+    //! Adaugam la noul user:
+    verificationToken: token,
+    verify: false,
   });
 
   return User.create(newUser);
